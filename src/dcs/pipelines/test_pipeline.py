8c4829c280ff78414ab1fdee2a8dfddfe20a64f6
João Correia <jfscorreia95@gmail.com>
2023-10-17 10:17:13 +0100

    [FIX] test_pipeline function

--- src/dcs/pipelines/test_pipeline.py
+++ src/dcs/pipelines/test_pipeline.py
@@ -1,17 +1,22 @@
+import time
+
 from deepmol.metrics import Metric
 from deepmol.pipeline_optimization import PipelineOptimization
 import optuna
 from sklearn.metrics import roc_auc_score
 
 
-def test_pipeline(train, valid):
-    evo_sampler = optuna.samplers.NSGAIISampler(population_size=10, mutation_prob=0.1,
-                                                crossover=None, crossover_prob=0.9,
-                                                swapping_prob=0.5, seed=None, constraints_func=None)
-    # AUROC
+def test_pipeline(train, valid, pipeline_name: str = None, seed: int = 1, optimizer: str = 'tpe'):
+    if optimizer == 'nsga2':
+        sampler = optuna.samplers.NSGAIISampler(seed=seed)
+    elif optimizer == 'tpe':
+        sampler = optuna.samplers.TPESampler(seed=seed)
+    else:
+        raise ValueError(f'Invalid optimizer: {optimizer}. It must be one of "nsga2" or "tpe"')
+    pipeline_name = pipeline_name or f'pipeline_{time.strftime("%Y_%m_%d-%H_%M_%S")}'
     metric = Metric(roc_auc_score)
-    pipeline = PipelineOptimization(sampler=evo_sampler,
-                                    study_name='test_pipeline',
+    pipeline = PipelineOptimization(sampler=sampler,
+                                    study_name=pipeline_name,
                                     direction='maximize')
     pipeline.optimize(train, valid, objective_steps='all', metric=metric, n_trials=10, save_top_n=3, data=train)
     return pipeline.best_pipeline
