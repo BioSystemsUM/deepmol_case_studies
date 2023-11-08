3d848d7379fe93e71045280eb4de02dee77d4235
João Correia <jfscorreia95@gmail.com>
2023-11-08 16:54:07 +0000

    [ADD] case studies to test + basic notebooks to check results

---
+++ scripts/tdc/cyp2c9_substrate/cyp2c9_substrate.py
@@ -0,0 +1,33 @@
+import time
+
+from deepmol.datasets import SmilesDataset
+from sklearn.metrics import precision_recall_curve, auc
+
+from dcs.pipelines import general_tdc_pipeline
+from dcs.utils import get_benchmark_group
+
+
+# TODO: check if works this way
+def area_under_precision_recall_curve(y_true, y_pred):
+    precision, recall, _ = precision_recall_curve(y_true=y_true, probas_pred=y_pred)
+    return auc(recall, precision)
+
+
+def run():
+    init_time = time.time()
+    group = get_benchmark_group['CYP2D6_Veith']
+    benchmark = group.get('CYP2D6_Veith')
+    train_val = benchmark['train_val']
+    data = SmilesDataset(smiles=train_val['Drug'].values, ids=train_val['Drug_ID'].values, y=train_val['Y'].values)
+    pipeline_name = 'cyp2d6'
+    storage = f'sqlite:///{pipeline_name}.db'
+    general_tdc_pipeline(pipeline_name=pipeline_name, group=group, tdc_dataset_name='CYP2D6_Veith',
+                         data_sample=data, seed=321, optimizer='tpe', storage=storage,
+                         metric=area_under_precision_recall_curve, direction='maximize', n_trials=100, save_top_n=100,
+                         trial_timeout=60 * 5)
+    final_time = time.time()
+    print(f'Elapsed time: {final_time - init_time}')
+
+
+if __name__ == '__main__':
+    run()
---
+++ scripts/tdc/cyp2d6/cyp2d6.py
@@ -0,0 +1,26 @@
+import time
+
+from deepmol.datasets import SmilesDataset
+from scipy.stats import spearmanr
+
+from dcs.pipelines import general_tdc_pipeline
+from dcs.utils import get_benchmark_group
+
+
+def run():
+    init_time = time.time()
+    group = get_benchmark_group['VDss_Lombardo']
+    benchmark = group.get('VDss_Lombardo')
+    train_val = benchmark['train_val']
+    data = SmilesDataset(smiles=train_val['Drug'].values, ids=train_val['Drug_ID'].values, y=train_val['Y'].values)
+    pipeline_name = 'vdss'
+    storage = f'sqlite:///{pipeline_name}.db'
+    general_tdc_pipeline(pipeline_name=pipeline_name, group=group, tdc_dataset_name='VDss_Lombardo',
+                         data_sample=data, seed=321, optimizer='tpe', storage=storage, metric=spearmanr,  # TODO: check metric
+                         direction='maximize', n_trials=100, save_top_n=100, trial_timeout=60 * 5)
+    final_time = time.time()
+    print(f'Elapsed time: {final_time - init_time}')
+
+
+if __name__ == '__main__':
+    run()
