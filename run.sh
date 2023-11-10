e6f3162acb11a8187feb74209eb6c90143688f4c
João Correia <jfscorreia95@gmail.com>
2023-11-10 12:09:55 +0000

    [FIX] sh file

--- run.sh
+++ run.sh
@@ -15,4 +15,4 @@ PYTHON_SCRIPT="$3"
 podman build . -t deepmol_case_study
 
 # Run the container with the specified Python script as the command
-podman run --rm -v /home/jcorreia/deepmol_case_studies/scripts/tdc/"$FOLDER_NAME"/:/workspace/scripts/tdc/"$FOLDER_NAME"/:z -d --device nvidia.com/gpu="$GPU_NUMBER" --security-opt=label=disable --name=="$FOLDER_NAME" deepmol_case_study /bin/bash -c "python $PYTHON_SCRIPT > output.txt"
+podman run --rm -v /home/jcorreia/deepmol_case_studies/scripts/tdc/"$FOLDER_NAME"/:/workspace/scripts/tdc/"$FOLDER_NAME"/:z -d --device nvidia.com/gpu="$GPU_NUMBER" --security-opt=label=disable --name="$FOLDER_NAME" deepmol_case_study /bin/bash -c "python $PYTHON_SCRIPT > output.txt"
