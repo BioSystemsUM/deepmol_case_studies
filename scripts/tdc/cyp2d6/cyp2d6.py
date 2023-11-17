[FIX] spearman and average_precision_score metrics

scripts/tdc/cyp2d6/cyp2d6.py
@@ -1,7 +1,7 @@
 import time
 
 from deepmol.datasets import SmilesDataset
-from scipy.stats import spearmanr
+from sklearn.metrics import average_precision_score
 
 from dcs.pipelines import general_tdc_pipeline
 from dcs.utils import get_benchmark_group
@@ -16,7 +16,7 @@ def run():
     pipeline_name = 'vdss'
     storage = f'sqlite:///{pipeline_name}.db'
     general_tdc_pipeline(pipeline_name=pipeline_name, group=group, tdc_dataset_name='VDss_Lombardo',
-                         data_sample=data, seed=321, optimizer='tpe', storage=storage, metric=spearmanr,  # TODO: check metric
+                         data_sample=data, seed=321, optimizer='tpe', storage=storage, metric=average_precision_score,
                          direction='maximize', n_trials=100, save_top_n=100, trial_timeout=60 * 5)
     final_time = time.time()
     print(f'Elapsed time: {final_time - init_time}')