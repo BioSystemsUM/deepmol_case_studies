00f0ce1cd77fde20ae1695eeb36a7f3237ab2556
João Correia <jfscorreia95@gmail.com>
2023-11-21 14:22:07 +0000

    [FIX] case studies names

--- scripts/tdc/cyp2d6/cyp2d6.py
+++ scripts/tdc/cyp2d6/cyp2d6.py
@@ -9,13 +9,13 @@ from dcs.utils import get_benchmark_group
 
 def run():
     init_time = time.time()
-    group = get_benchmark_group['VDss_Lombardo']
-    benchmark = group.get('VDss_Lombardo')
+    group = get_benchmark_group['CYP2D6_Veith']
+    benchmark = group.get('CYP2D6_Veith')
     train_val = benchmark['train_val']
     data = SmilesDataset(smiles=train_val['Drug'].values, ids=train_val['Drug_ID'].values, y=train_val['Y'].values)
-    pipeline_name = 'vdss'
+    pipeline_name = 'cyp2d6'
     storage = f'sqlite:///{pipeline_name}.db'
-    general_tdc_pipeline(pipeline_name=pipeline_name, group=group, tdc_dataset_name='VDss_Lombardo',
+    general_tdc_pipeline(pipeline_name=pipeline_name, group=group, tdc_dataset_name='CYP2D6_Veith',
                          data_sample=data, seed=321, optimizer='tpe', storage=storage, metric=average_precision_score,
                          direction='maximize', n_trials=100, save_top_n=100, trial_timeout=60 * 5)
     final_time = time.time()
