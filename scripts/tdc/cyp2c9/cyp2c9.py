4448825ec0b6cd2229a7953309d5134013de1413
jcorreia11 <jfscorreia95@gmail.com>
2023-11-22 10:23:12 +0000

    change seed

--- scripts/tdc/cyp2c9/cyp2c9.py
+++ scripts/tdc/cyp2c9/cyp2c9.py
@@ -16,7 +16,7 @@ def run():
     pipeline_name = 'cyp2c9'
     storage = f'sqlite:///{pipeline_name}.db'
     general_tdc_pipeline(pipeline_name=pipeline_name, group=group, tdc_dataset_name='CYP2C9_Veith',
-                         data_sample=data, seed=321, optimizer='tpe', storage=storage,
+                         data_sample=data, seed=4321, optimizer='tpe', storage=storage,
                          metric=average_precision_score, direction='maximize', n_trials=100, save_top_n=100,
                          trial_timeout=60 * 5)
     final_time = time.time()
